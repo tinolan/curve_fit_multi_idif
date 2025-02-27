6d92cdbe2c321ab45b83ed4d2ef0fc91ad59170d
tinolan <39703888+tinolan@users.noreply.github.com>
2025-02-27 23:16:19 +0100

    Add files via upload

---
+++ utils/set_root_paths.py
@@ -0,0 +1,13 @@
+import os
+
+root_path = "/home/guests/valentin_langer/code/portal_input_alpha"
+print("root_path =",root_path)
+
+root_checkpoints_path = "/home/guests/valentin_langer/data/checkpoint/portal_input_alpha"
+print("root_checkpoints_path =",root_checkpoints_path)
+
+root_data_path = "/home/polyaxon-data/data1"
+print("root_data_path =", root_data_path)
+
+root_idif_path = "/home/guests/valentin_langer/data/IDIF"
+print("root_idif_path =", root_idif_path)
