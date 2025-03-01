0e0bb8cd9ed36729837705c5b3dcecc5d16b2d54
tinolan <39703888+tinolan@users.noreply.github.com>
2025-03-01 13:29:50 +0100

    Update set_root_paths.py

--- utils/set_root_paths.py
+++ utils/set_root_paths.py
@@ -1,13 +1,13 @@
 import os
 
-root_path = "/home/guests/valentin_langer/code/portal_input_alpha"
+root_path = "/xxx/xxx/xxx/code/portal_input_alpha"
 print("root_path =",root_path)
 
-root_checkpoints_path = "/home/guests/valentin_langer/data/checkpoint/portal_input_alpha"
+root_checkpoints_path = "/xxx/xxx/xxx/data/checkpoint/portal_input_alpha"
 print("root_checkpoints_path =",root_checkpoints_path)
 
-root_data_path = "/home/polyaxon-data/data1"
+root_data_path = "/xxx/xxx-xxx/xxx"
 print("root_data_path =", root_data_path)
 
-root_idif_path = "/home/guests/valentin_langer/data/IDIF"
+root_idif_path = "/xxx/xxx/xxx/data/IDIF"
 print("root_idif_path =", root_idif_path)
